12514783656a0b2296ec20456bf8bff650a60093
Diwaz <diwaswatson@gmail.com>
2025-10-11 11:10:33 +0545

    sandbox runs with init vite project and display publicly accesible url

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import { Sandbox } from '@e2b/code-interpreter'
+
+const viteScript = `
+  npm create vite@latest vite-app --y
+  cd vite-app
+  npm install
+  npm run build
+  npm install serve --save-dev
+  npx serve -s dist -l 5555
+`;
+
+const sandbox = await Sandbox.create()
+
+// console.log("Vite setup logs:", execution.logs);
+const host = sandbox.getHost(5555)
+console.log(`https://${host}`)
+await sandbox.files.write(
+  'run-vite.sh',
+  viteScript,
+);
+// You need to always pass a port number to get the host
+await sandbox.commands.run(
+  'sh run-vite.sh',
+);
+
